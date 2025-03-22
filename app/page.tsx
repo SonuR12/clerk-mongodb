0d2c27427f1b054849300ffe1c22afba4c7ed1be
Sonu Rai <sonukumar9905757@gmail.com>
2025-03-22 22:55:10 +0530

    data show

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
+"use client"
+import { useUser } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+
   return (
     <div className="flex items-center justify-center h-full">
-      <div className="text-2xl">Home Page</div>
+      <div className="text-2xl">Hello, {user.firstName} welcome to Home Page</div>
     </div>
   );
 }
